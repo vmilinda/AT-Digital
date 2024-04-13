b1842d8d21541cd1421467031c91ed4f89552591
V_rajmilinda <158337367+vmilinda@users.noreply.github.com>
2024-04-13 09:50:32 +0530

    five commit

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let menuIcon = document.querySelector('#menu-icon');
+let nav = document.querySelector('.nav');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    nav.classList.toggle('active');
+};
+
+
+
 // ----------------------------------------------------FAQ section ------------------------------------------------------
 
 const faqs = document.querySelectorAll(".faq");
